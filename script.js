ca3b419f8a87a32592193c79ce3418ca199a46ec
Clov23 <gerardodev408@outlook.com>
2026-03-13 12:39:54 -0600

    pie de pagina

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 document.getElementById('videojuego').addEventListener('click', function() {
     document.getElementById('titulo').style.color = red;
 });
+
+function cargarFooter() {
+  var footer = document.getElementById("footer");
+  footer.innerHTML = `
+    <p><strong>Authors:</strong> Todo el team Luddies </p>
+    <p><strong>Email:</strong> contact@luddies.fake</p>
+    <p><strong>Project:</strong> Videojuegos — AD-07 Group CH65</p>
+  `;
+}
